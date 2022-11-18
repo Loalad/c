4b433f6a84a2f64f9af3c95d861cd363439aef1c
Loalad <112859224+Loalad@users.noreply.github.com>
2022-11-18 16:50:24 +0800

    Add files via upload

---
+++ 4OJ/cmake-build-debug/CMakeFiles/4OJ.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "4OJ.exe"
+  "4OJ.exe.manifest"
+  "4OJ.pdb"
+  "CMakeFiles/4OJ.dir/final.c.obj"
+  "CMakeFiles/4OJ.dir/main.c.obj"
+  "lib4OJ.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/4OJ.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 5OJ/cmake-build-debug/CMakeFiles/5OJ.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "5OJ.exe"
+  "5OJ.exe.manifest"
+  "5OJ.pdb"
+  "CMakeFiles/5OJ.dir/main.c.obj"
+  "lib5OJ.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/5OJ.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
